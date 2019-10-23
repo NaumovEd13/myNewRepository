a6a61786d73d2d3d426a5b0839685e7a7858311a
Eduard Naumov <edn.rzn@gmail.com>
2019-10-23 22:12:11 +0300

    new commit

---
+++ 8_2.kt
@@ -0,0 +1,61 @@
+import javafx.application.Application.launch
+import kotlinx.coroutines.*
+
+class Pet(val nickName: String, val mealDuration: Long) {
+    suspend fun eat() {
+        delay(mealDuration)
+        println("$nickName покушал")
+    }
+}
+
+suspend fun main(args: Array<String>) {
+
+    val cat = Pet("Васька", 3000)
+    val dog = Pet("Шарик", 2000)
+    val mouse = Pet("Камыш", 4000)
+    val hampster = Pet("Леонид", 5000)
+    val bird = Pet("Жорик", 1000)
+
+    runBlocking {
+        launch {
+            cat.eat()
+        }
+        launch {
+            dog.eat()
+        }
+        launch {
+            mouse.eat()
+        }
+        launch {
+            hampster.eat()
+        }
+        launch {
+            bird.eat()
+        }
+    }
+    println("Животные поели")
+    delay(5000)
+
+    coroutineScope {
+        launch {
+            cat.eat()
+        }
+        launch {
+            dog.eat()
+        }
+        launch {
+            mouse.eat()
+        }
+        launch {
+            hampster.eat()
+        }
+        launch {
+            bird.eat()
+        }
+        println("Еду раздали")
+    }
+    println("Животные поели")
+
+
+}
+
