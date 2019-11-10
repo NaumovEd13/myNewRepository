3ed3caea585fe64559f9d5c7338a5e723fc6d477
NaumovEd13 <55740830+NaumovEd13@users.noreply.github.com>
2019-11-10 21:53:17 +0300

    Update 12_3_1.kt

--- 12_3_1.kt
+++ 12_3_1.kt
@@ -1,9 +1,6 @@
 fun main(){
     var list:List<String> = mutableListOf("Хрюша", "Степаша", "Филя", "Гуля")
     var max=list.get(0)
-    var maxLengthName=list.find {
-        it.length>max.length
-
-    }
+    var maxLengthName=list.find {it.length>max.length}
     println(maxLengthName)
 }
