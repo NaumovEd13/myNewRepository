2cb502b0461d07ebfd21a5087b0dc9b1a5d32b4c
NaumovEd13 <55740830+NaumovEd13@users.noreply.github.com>
2019-11-21 09:23:54 +0300

    Update 12_3_2.kt

--- 12_3_2.kt
+++ 12_3_2.kt
@@ -1,6 +1,5 @@
 fun main(){
     var list:List<String> = mutableListOf("Хрюша", "Степаша", "Филя", "Олег Монгол")
-    var max = list.asSequence().maxBy { it.length }
+    var max=list.asSequence().find { it.equals(list.asSequence().maxBy { it.length }) }
     println(max)
-
 }
